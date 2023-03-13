8ca5261099bc5cb3b00add33d38728e572da3bc9
Mohanad.M.Ali <mohanad1213@live.com>
2023-03-14 00:45:57 +0400

    fix the project

---
+++ api/Coffast/Controllers/ValueController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffast.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly ILogger<ValueController> _logger;
+
+        public ValueController(ILogger<ValueController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public ActionResult<object> Do()
+        {
+            this._logger.LogInformation("you code is working file");
+            return "Start Working";
+        }
+    }
+}
